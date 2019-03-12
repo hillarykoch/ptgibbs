b899519d8d4e14ad1045e697befd0ae2d7741f57
Hillary Koch <hillary.koch01@gmail.com>
2019-03-12 13:24:31 -0400

    add eigenvector plotter

--- src/plotters.jl
+++ src/plotters.jl
@@ -43,3 +43,33 @@ function plot_corr(chain, m::Int64, labs::Array{String,1}; reorder = true, key =
         Guide.colorkey(title=""),
         key ? Theme(key_position = :right) : Theme(key_position = :none))
 end
+
+export plot_eigvec
+function plot_eigvec(chain, m::Int64, labs::Array{String,1}, eig_num::Int64; pal=missing)
+        Schain = get_Sigma_chain(chain, m)
+        covmat = mapslices(x -> mean(x), Schain; dims = 3)[:,:,1]
+        evals, evecs = (eigvals(covmat), eigvecs(covmat))
+
+        # Make names dataframe so plot has names
+        # The kth ROW is the kth eigenvector here
+        edf = DataFrame(evecs', Symbol.(labs))
+        edf.vec = 1:size(covmat, 1)
+        molten = melt(edf)
+
+        # Plot first eigenvector
+        if ismissing(pal)
+                p = Gadfly.plot(molten[molten.vec .== eig_num, :],
+                        x=:variable,
+                        y=:value,
+                        Guide.xlabel(nothing), Guide.ylabel(nothing),
+                        Geom.bar)
+        else
+                p = Gadfly.plot(molten[molten.vec .== eig_num, :],
+                        x=:variable,
+                        y=:value,
+                        color=pal,
+                        Guide.xlabel(nothing), Guide.ylabel(nothing),
+                        Geom.bar)
+        end
+        (p, sort(evals; rev=true))
+end

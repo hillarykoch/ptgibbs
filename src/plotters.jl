486b0232f6e2bd92bd98462fe3c400bc4a7c3208
Hillary Koch <hillary.koch01@gmail.com>
2019-03-13 13:42:17 -0400

    update plotters with mean effect plot

--- src/plotters.jl
+++ src/plotters.jl
@@ -7,6 +7,8 @@ using DataFrames
 import StatsBase: cov2cor
 import RLEVectors: rep
 
+include("helpers.jl")
+
 export plot_corr
 function plot_corr(chain, m::Int64, labs::Array{String,1}; reorder = true, key = false, min_value = -1, max_value = 1, linkage = :ward)
 """
@@ -62,14 +64,51 @@ function plot_eigvec(chain, m::Int64, labs::Array{String,1}, eig_num::Int64; pal
                         x=:variable,
                         y=:value,
                         Guide.xlabel(nothing), Guide.ylabel(nothing),
-                        Geom.bar)
+                        Geom.bar)#,
+                        #yintercept = 0,
+                        #Geom.hline(color="black", style = :dash))
         else
                 p = Gadfly.plot(molten[molten.vec .== eig_num, :],
                         x=:variable,
                         y=:value,
                         color=pal,
                         Guide.xlabel(nothing), Guide.ylabel(nothing),
-                        Geom.bar)
+                        Geom.bar)#,
+                        #yintercept = 0,
+                        #Geom.hline(color="black", style = :dash))
         end
         (p, sort(evals; rev=true))
+end
+
+function plot_effects(chain, obsidx::Int64, labs::Array{String,1}, M::Int64; pal = missing)
+        z_chain = get_z_chain(chain)
+        #MAPs = mapslices(x -> [ mean(x .== y) for y in sort(unique(z_chain)) ], z_chain; dims = 2)
+        MAPs = mapslices(x -> [ mean(x .== y) for y in 1:M ], z_chain; dims = 2)
+        emap = MAPs[obsidx,:]
+
+        # Effect size estimates in each cell type,
+        #       weighted by the posterior probability the this site is in any given cluster
+        #       plus the standard errors
+        emeans = mapslices(x -> sum(x), hcat([ mu_ests[m,:] .* emap[m] for m in 1:M ]...)'; dims = 1)[1,:]
+        eses = mapslices(y -> sum(y), hcat([ mapslices(x -> std(x), get_mu_chain(chain, m);
+                                dims = 2) .* emap[m] for m in 1:M ]...)'; dims = 1)[1,:]
+
+
+        # Plot first eigenvector
+        if ismissing(pal)
+                Gadfly.plot(y = labs, x = emeans,
+                                xmin = emeans .- eses,
+                                xmax = emeans .+ eses,
+                                Geom.point,
+                                Guide.xlabel(nothing), Guide.ylabel(nothing),
+                                Geom.xerrorbar)
+        else
+                Gadfly.plot(y = labs, x = emeans,
+                                xmin = emeans .- eses,
+                                xmax = emeans .+ eses,
+                                Geom.point,
+                                Geom.xerrorbar,
+                                Guide.xlabel(nothing), Guide.ylabel(nothing),
+                                color = pal)
+        end
 end
